dec19df7a47562dca0f041ee4ce476c2a15a394e
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-07-10 12:36:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 popular_df = pickle.load(open('popular.pkl', 'rb'))
 pt = pickle.load(open('pt.pkl', 'rb'))
-books = pickle.load(open('books.pkl', 'rb'))
+books = pickle.load(open('books.pkl', 'rb')) 
 similarity_score = pickle.load(open('similarity_scores.pkl', 'rb'))
 
 app = Flask(__name__)
